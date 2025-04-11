6d5c2097af691c5b374c5735143cf446f2717e5f
prasantaaws1 <105577264+prasantaaws1@users.noreply.github.com>
2025-04-12 04:25:27 +0530

    Update main.tf

--- infrastructure/modules/ecs/main.tf
+++ infrastructure/modules/ecs/main.tf
@@ -17,6 +17,7 @@ data "template_file" "ecs_app" {
 }
 
 resource "aws_ecs_task_definition" "app" {
+  
   family                   = "ecs-app-task"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
